2181870416949a14d413044507c8550f4d54ce6c
hungphan <colorvisavn@gmail.com>
2019-08-19 15:56:15 +0800

    Introduce site.yml

--- run-playbooks.sh
+++ run-playbooks.sh
@@ -3,10 +3,7 @@
 set -e # exit on failure
 set -x # log all executed commands
 
-ansible-playbook control.yml
-ansible-playbook database.yml
-ansible-playbook webserver.yml
-ansible-playbook loadbalancer.yml
+ansible-playbook site.yml
 ansible-playbook stack_status.yml
 
 set +x
