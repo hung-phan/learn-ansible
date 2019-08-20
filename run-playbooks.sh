b89255ff7580224f3d554f6b14ebcd44ca4094bb
hungphan <colorvisavn@gmail.com>
2019-08-20 14:56:41 +0800

    Specify ask-vault-pass for stack_status

--- run-playbooks.sh
+++ run-playbooks.sh
@@ -4,7 +4,7 @@ set -e # exit on failure
 set -x # log all executed commands
 
 ansible-playbook --ask-vault-pass site.yml
-ansible-playbook stack_status.yml
+ansible-playbook --ask-vault-pass stack_status.yml
 
 set +x
 set +e
